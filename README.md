c399cd7524847e0e6e25574e537702e1e801c9e1
Andela-eugene <eugene.liyai@andela.com>
2019-08-10 10:36:25 +0300

    feat: finish sql implementation

---
+++ README.md
@@ -0,0 +1,8 @@
+### SQL Solution
+Under sql folder.
+
+### Data Structure Solution
+Under linkedList folder.
+
+### Web Solution
+Under web folder
---
+++ sql/cars.sql
@@ -0,0 +1,23 @@
+-- Database implementation using postgres
+
+--  create database cars
+CREATE DATABASE Cars
+
+-- create table car sales
+CREATE TABLE CarSales (
+    id SERIAL PRIMARY KEY,
+    CarMaker VARCHAR (50) NOT NULL,
+    CarModel VARCHAR (200) NOT NULL,
+    TotalSalesPrice NUMERIC (7, 2) NOT NULL,
+    DateCreated TIMESTAMP
+);
+
+-- insert values into car sales
+INSERT INTO CarSales (CarMaker, CarModel, TotalSalesPrice)
+VALUES
+    ('Maker A', 'A Model X V6', 100000), 
+    ('Maker B', 'B Model Z V6', 200000), 
+    ('Maker C', 'C Model Y 4000', 100000), 
+
+-- query total price per model
+SELECT CarMaker, CarModel, TotalSalesPrice FROM CarSales WHERE DateCreated > now() - interval '30 days';
